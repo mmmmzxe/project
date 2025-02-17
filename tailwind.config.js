58e95ef94725eb3e4c5604a86b78f2e4522341e9
mmmmzxe <maryem.mostafa00@gmail.com>
2025-02-17 17:57:44 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+export default {
+    theme: {
+      backgroundImage: {
+        pattern: "url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSeFuLAe8NkoCAuWoFsFnkaocC6pUhUqha13Q&s')",
+        hero: "url('assets/img/hero/sabrianna-uiKSc7-NM2s-unsplash.jpg')",
+      
+      
+        newsletter: "url('assets/img/newsletter/bg.png')",
+        footer: "url('assets/img/footer/bg2.jpg')",
+      },
+      extend: {
+
+        colors: {
+          'primary': '#ff49db',
+        },
+     
+      
+        fontFamily: {
+          'sans': ['Helvetica', 'Arial', 'sans-serif'],
+        },
+      },
+    },
+    plugins: [],
+  }
